efa75de0f6e36b214466537574af994fa1f4ad61
Shatalov.AG <mrstep93@ya.ru>
2024-02-25 00:29:32 +0300

    Добавьте файлы проекта.

---
+++ Module12.Task1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Module12.Task1
+{
+    internal class Program
+    {
+        static List<User> users;
+
+        static void Main(string[] args)
+        {
+            CreateUsers();
+            Console.WriteLine("Hello, GUEST!");
+            Console.Write("Write your login: ");
+            string currentlogin = Console.ReadLine();
+            User currentUser = null;
+            try
+            {
+                currentUser = User.CheckUser(currentlogin, in users);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                currentUser =User.CreateNew(currentlogin);
+                users.Add(currentUser);
+            }
+
+            Greetings(in currentUser);
+        }
+        
+        static void CreateUsers()
+        {
+            users = new()
+            {
+                new User("mrstep", "Aleksey", true),
+                new User("makendorf", "Lexa", false),
+                new User("alison", "kote", true),
+                new User("stalker", "Pyotr sergeevich", false)
+            };
+        }
+
+        static void Greetings(in User user)
+        {
+            Console.WriteLine($"Привет, {user.Name}!!! Рады видеть вас снова");
+            if (!user.IsPremium)
+            {
+                ShowAds();
+            } else
+            {
+                Console.WriteLine("Спасибо, что пользуетесь премиальным сервисом!");
+            }
+        }
+
+        static void ShowAds()
+        {
+            string[] advMessages = 
+                { "Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com",
+                "Купите подписку на МыКомбо и слушайте музыку везде и всегда!",
+                "Спишите долги легально и навсегда -->",
+                "Боль уйдет навсегда, стоит лишь применить один старый советский р..."
+                };
+
+            Random random = new Random();
+            Console.WriteLine(advMessages[random.Next(0,4)]);
+            // Остановка на 1 с
+            Thread.Sleep(3000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Module12.Task1/Users.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module12.Task1
+{
+    public class User
+    {
+        public string Login { get; }
+        public string Name { get; private set; }
+        public bool IsPremium { get; private set; }
+
+        public User(string login, string name, bool isPremium)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = isPremium;
+        }
+    
+        public static User CheckUser(string _login, in List<User> userList)
+        {
+            bool success = false;
+            foreach (var user in userList)
+            {
+                if (user.Login == _login)
+                {
+                    success = true;
+                    return user;
+                }
+            }
+            if (!success)
+            {
+                throw new Exception("Пользователь с таким логином не найден");
+            }
+            return null;
+        }
+
+        public static User CreateNew(string _login)
+        {
+            Console.WriteLine("Давайте создадим вашу учетную запись");
+            Console.Write("Введите ваше имя: ");
+            string _name = Console.ReadLine();
+            User newUser = new User(_login, _name, false);
+            Console.WriteLine("Учетная запись создана");
+            return newUser;
+        }
+    }
+}
